a924c3a3f42c8606d4c533447ea46bd9dbd307a3
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-04-06 16:42:57 +0300

    поправила фильтр

--- ProductShop/ListPage.xaml.cs
+++ ProductShop/ListPage.xaml.cs
@@ -83,14 +83,15 @@ namespace ProductShop
                 filterProd = filterProd.OrderByDescending(c => c.Name);
             }
 
-            if (cb_date.SelectedIndex == 1)
+            if (cb_date.SelectedIndex == 0)
             {
                 filterProd = filterProd.OrderBy(c => c.AddDate);
             }
-            else
+            else if(cb_date.SelectedIndex == 1)
             {
                 filterProd = filterProd.OrderByDescending(c => c.AddDate);
             }
+            
 
             if (cb_mounth.IsChecked.GetValueOrDefault())
             {
